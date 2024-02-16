f8214e17c04fd6beb552b9118b29d3d614db5f69
2002Manaswini <2002manaswini@.com>
2024-02-16 17:52:30 +0530

    first commit

---
+++ send_html.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+FAI=Flask(__name__)
+
+
+@FAI.route('/send_html')
+def send_html():
+    return render_template('render.html')
+
+    
+if __name__=='__main__':
+    FAI.run(debug=True)
